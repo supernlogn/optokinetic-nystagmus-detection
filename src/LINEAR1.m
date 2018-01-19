de02032fce039c1cba363cef26b2d92158e3cb37
supernlogn <ioan.athanasiadis@gmail.com>
2018-01-19 20:52:11 +0200

    Linear separation

---
+++ src/LINEAR1.m
@@ -0,0 +1,20 @@
+if(BASIC)
+    plot(T, X1);
+end
+% see if the signal has information
+% if voids and non-voids exist inside it
+% then there is information.
+% plot(T, log(abs(X1) + eps));
+% Y1 = log(abs(X1(2:end))) - log(abs(X1(1:LENGTH-1)));
+% plot(T(1:end-1), Y1)
+
+filterorder = 10;
+% we tested 30, 20, 10, 5
+% 10 is much more promising...
+nsam = 20;
+timesep = 13;
+% get EXTREMA SERIES 
+[AMA, AMI, AMD, TMA, TMI, TBP] = produce_time_series(X1, Ts, filterorder, nsam, timesep, EXTREMA_PLOT, EXTREMA_SERIES_PLOT, FIR);
+
+% all linear analysis
+linear_analysis(AMA, 'AMA', MAX_ORDER, POLORDER, EXTREMA_PLOT, EXTREMA_SERIES_PLOT, REMOVES_TREND, AUTOCORR_PLOT, AKAIKE);
--- src/main.m
+++ src/main.m
@@ -25,48 +25,7 @@ fs = 100; % Hz
 Ts = 1.0 / fs; % seconds
 
 %%%%%%%%%%%%%%%% Time Series 1 %%%%%%%%%%%%%%%%
-
-if(BASIC)
-    plot(T, X1);
-end
-% see if the signal has information
-% if voids and non-voids exist inside it
-% then there is information.
-% plot(T, log(abs(X1) + eps));
-% Y1 = log(abs(X1(2:end))) - log(abs(X1(1:LENGTH-1)));
-% plot(T(1:end-1), Y1)
-
-filterorder = 10;
-% we tested 30, 20, 10, 5
-% 10 is much more promising...
-nsam = 20;
-timesep = 13;
-% get EXTREMA SERIES 
-[AMA, AMI, AMD, TMA, TMI, TBP] = produce_time_series(X1, Ts, filterorder, nsam, timesep, EXTREMA_PLOT, EXTREMA_SERIES_PLOT, FIR);
-
-% all linear analysis
-linear_analysis(AMA, 'AMA', MAX_ORDER, POLORDER, EXTREMA_PLOT, EXTREMA_SERIES_PLOT, REMOVES_TREND, AUTOCORR_PLOT, AKAIKE);
-
+LINEAR1
 
 %%%%%%%%%%%%%%%% Time Series 2 %%%%%%%%%%%%%%%%
-
-if(BASIC)
-    plot(T, X2);
-end
-% see if the signal has information
-% if voids and non-voids exist inside it
-% then there is information.
-% plot(T, log(abs(X1) + eps));
-% Y1 = log(abs(X1(2:end))) - log(abs(X1(1:LENGTH-1)));
-% plot(T(1:end-1), Y1)
-
-filterorder = 5;
-% we tested 30, 20, 10, 5
-% 5 is much more promising...
-nsam = 20;
-timesep = 13;
-% get EXTREMA SERIES 
-[AMA, AMI, AMD, TMA, TMI, TBP] = produce_time_series(X2, Ts, filterorder, nsam, timesep, EXTREMA_PLOT, EXTREMA_SERIES_PLOT, FIR);
-
-% all linear analysis
-linear_analysis(AMA, 'AMA', MAX_ORDER, POLORDER, EXTREMA_PLOT, EXTREMA_SERIES_PLOT, REMOVES_TREND, AUTOCORR_PLOT, AKAIKE);
+LINEAR2
